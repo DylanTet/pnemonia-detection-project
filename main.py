750b59be7eefe23c50e2aae0638360780d890194
Dylan Tettemer <tettemerd@gmail.com>
2022-12-11 07:45:09 -0800

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastai import *
+from fastai.vision import *
+from fastai.metrics import error_rate
+import os
+import pandas as pd
+import numpy as np
+
+x = '/Users/dylantettemer/Downloads/chest_xray/train'
+path = os.path(x)
+path.ls()
+
